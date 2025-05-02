b4925ec6bd821ae08c457d8055f5c4a904b47d18
gaurav9508 <gaurav.soni9508@gmail.com>
2025-05-02 22:26:36 +0530

    Initial commit for new repo

---
+++ app/page.js
@@ -0,0 +1,36 @@
+"use client"
+import { useEffect } from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+
+  const {user} = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+
+  useEffect(() => {
+    user && checkUser();
+  }, [user]);
+
+  const checkUser = async () => {
+    const result = await createUser({
+      email: user?.primaryEmailAddress?.emailAddress,
+      imageUrl: user?.imageUrl,
+      userName: user?.fullName
+    });
+
+    console.log(result);
+  }
+  return (
+    <div>
+      <h2>Welcome to PDFIntellect</h2>
+      <Button>Click Me</Button>
+
+      <UserButton/>
+    </div>
+  );
+}
