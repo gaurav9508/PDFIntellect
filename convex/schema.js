71b4b1ffe6f5344f3f9200b2b7ed571ebc584b6b
gaurav9508 <gaurav.soni9508@gmail.com>
2025-05-02 22:34:13 +0530

    added schema

--- convex/schema.js
+++ convex/schema.js
@@ -2,7 +2,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    user:defineTable({
+    users:defineTable({
         userName:v.string(),
         email:v.string(),
         imageUrl:v.string()
